0ee3946ec170ccd3ed160142c327ef70bb1f7d81
George Pilitsoglou <george.pilitsoglou@gmail.com>
2024-08-22 11:46:01 +0300

    chore: added sale state

--- src/shared/context/pos.context.tsx
+++ src/shared/context/pos.context.tsx
@@ -8,19 +8,34 @@ const CashierStateUpdater = createContext<
 >(undefined);
 
 const SalesContext = createContext<Sale[] | null | undefined>(undefined);
-const SalesStateUpdater = createContext<
-  React.Dispatch<React.SetStateAction<Sale[] | null>> | undefined
->(undefined);
+const SalesStateUpdater = createContext<((sale: Sale) => void) | undefined>(undefined);
 
 type PosContextProviderProps = Readonly<PropsWithChildren>;
 export function PosContextProvider({ children }: PosContextProviderProps) {
   const [cashier, setCashier] = useState<Cashier | null>(null);
-  const [sales, setSales] = useState<Sale[] | null>(null);
+  const [sales, setSales] = useState<Sale[] | null>(() => {
+    const savedSales = localStorage.getItem("sales");
+    const initiaSales: Sale[] = savedSales ? JSON.parse(savedSales) : null;
+    return initiaSales;
+  });
+
+  const updateSales = (newSale: Sale) => {
+    if (!sales) {
+      return;
+    }
+
+    const updatedSaleIndex = sales?.findIndex((sale) => sale.cashierId === newSale.cashierId);
+
+    const updatedSales = sales?.splice(updatedSaleIndex, 1, newSale);
+    setSales(updatedSales);
+    localStorage.setItem("sales", JSON.stringify(updatedSales));
+  };
+
   return (
     <CashierContext.Provider value={cashier}>
       <CashierStateUpdater.Provider value={setCashier}>
         <SalesContext.Provider value={sales}>
-          <SalesStateUpdater.Provider value={setSales}>{children}</SalesStateUpdater.Provider>
+          <SalesStateUpdater.Provider value={updateSales}>{children}</SalesStateUpdater.Provider>
         </SalesContext.Provider>
       </CashierStateUpdater.Provider>
     </CashierContext.Provider>
@@ -42,4 +57,12 @@ export function useCashierStateUpdater() {
   }
 
   return cashierStateUpdate;
+}
+
+export function useSalesState() {
+  const salesState = useContext(SalesContext);
+  if (typeof salesState === "undefined") {
+    throw new Error("useSalesState must be used withing CashierState");
+  }
+  return salesState;
 }
