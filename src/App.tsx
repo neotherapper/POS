411fcc7b4842a352416107264337db3b3e088d6c
George Pilitsoglou <george.pilitsoglou@gmail.com>
2024-08-22 22:38:12 +0300

    chore: removed not used imports at App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import Link from "@mui/material/Link";
 import SelectCashier from "./SelectCashier";
 import { PosContextProvider } from "./shared/context/pos.context";
 import SalesDashboard from "./shared/components/SalesDashboard";
