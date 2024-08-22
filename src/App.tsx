f1c3bf5f6eb8b1110c7d658fa3379d58c55c2957
George Pilitsoglou <george.pilitsoglou@gmail.com>
2024-08-22 11:46:29 +0300

    chore: setup localStorage to save data

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,18 @@ import Link from "@mui/material/Link";
 import SelectCashier from "./SelectCashier";
 import { PosContextProvider } from "./shared/context/pos.context";
 import SalesDashboard from "./shared/components/SalesDashboard";
+import Cashiers from "../data/cashier.json";
+import Products from "../data/products.json";
+import Sales from "../data/sales.json";
+import { useEffect } from "react";
 
 export default function App() {
+  useEffect(() => {
+    localStorage.setItem("cashiers", JSON.stringify(Cashiers));
+    localStorage.setItem("products", JSON.stringify(Products));
+    localStorage.setItem("sales", JSON.stringify(Sales));
+  }, []);
+
   return (
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
